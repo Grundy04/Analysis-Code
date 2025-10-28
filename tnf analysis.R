f6d63828251410508244451cb88e970ef7c194de
Grundy04 <danielgrundy2004@gmail.com>
2025-10-28 12:07:12 +0000

    Add files via upload

--- tnf analysis.R
+++ tnf analysis.R
@@ -7,9 +7,9 @@ for(i in packages){
 }
 
 setwd("D:/MscR - Neuroinflammation/Results/R coding")
-tnf = read.csv("TNF2.csv")
+tnf = read.csv("TNF1.csv")
 
-###################################fuck with it######################
+###################################mess with it######################
 
 tnf = tnf %>%
   mutate(Condition = factor(Condition,
@@ -32,11 +32,11 @@ for (i in 1:2) { assign(paste0("DMSO_av_", i),
 
 tnf_all_workable = tnf %>%
   #Normalise each experiment to their respective DMSO
-  mutate(Normalised = case_when(Experiment == 1 ~ (Standardised / DMSO_av_1)*100
+  mutate(Normalised = case_when(Experiment == 1 ~ (DMSO_av_1 / Standardised)*100
 ))
                                 
-tnf_all_workable = tnf_all_workable %>% 
-  mutate(Normalised = round(Normalised, 4))
+#tnf_all_workable = tnf_all_workable %>% 
+#  mutate(Normalised = round(Normalised, 4))
 
 #tnf_workable$Condition = forcats::fct_rev(IL8_workable$Condition)
 
@@ -45,12 +45,18 @@ tnf_all_workable = tnf_all_workable %>%
 tnf_all_workable %>%
   ggplot(aes(x=Condition, y=Normalised))+
   geom_bar(stat = "identity")+
-  geom_point(data = tnf_all_workable, mapping = aes(x=Condition, y=Normalised))+
-  geom_text(aes(label = Normalised), 
-            vjust = -0.5, 
-            size = 3.5, 
-            fontface = "bold", 
-            color = "black") +
+  #geom_point(data = tnf_all_workable, mapping = aes(x=Condition, y=Normalised))+
+  #geom_text(aes(label = Normalised), 
+  #          vjust = -0.5, 
+  #          size = 3.5, 
+  #          fontface = "bold", 
+  #          color = "black") +
+  scale_x_discrete(labels = c("DMSO" = "DMSO",
+                              "Bay" = "BAY",
+                              "TAT14 100um" = "TAT14 100\u00B5M",
+                              "Omaveloxolone 1um" = "Omaveloxolone 1 \u00B5M",
+                              "PROTAC 100um" = "PROTAC 100 \u00B5M",
+                              "PROTAC 200um" = "PROTAC 200 \u00B5M"))+
   theme_classic()+
   theme(axis.text = element_text(size=11,
                                  colour = "black",
@@ -58,9 +64,7 @@ tnf_all_workable %>%
         axis.title.y = element_text(size=12,
                                     colour = "black",
                                     face="bold"),
-        axis.title.x = element_text(size=12,
-                                    colour = "black",
-                                    face = "bold"),
+        axis.title.x = element_blank(),
         axis.line = element_line(linewidth = 1),
         axis.ticks = element_line(linewidth = 1),
         axis.text.x = element_text(angle=45,
@@ -69,8 +73,7 @@ tnf_all_workable %>%
                                   size=12,
                                   face="bold",
                                   hjust = 0.5))+
-  geom_hline(yintercept = 0, linetype = "dashed", color = "red", linewidth = 1)+
-  ylab("Tnf-alpha inhibtion (%)\nnormalised to DMSO")+
-  xlab("Conditions")+
-  scale_y_continuous(limits = c(0,250),expand = c(0,0))+
-  ggtitle("Tnf-alpha ELISA")
+  ylab("TNF-\u03B1 secretion (%)\nnormalised to DMSO")+
+  #xlab("Second Extracts")+
+  scale_y_continuous(limits = c(0,100),expand = c(0,0))+
+  ggtitle("TNF-\u03B1 ELISA First Extract")
